82791cb134188ddd80093fe6f63e0b17d75db1c9
Quent1nl <56486588+Quent1nl@users.noreply.github.com>
2020-03-07 17:29:28 +0100

    Modif show view et ajout api

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,3 +1,7 @@
 class Burger < ApplicationRecord
-    
+    def nutriments
+        code = "3029330003533"
+        product = Openfoodfacts::Product.get(code, locale: 'fr')
+        return product.nutriments
+    end
 end
