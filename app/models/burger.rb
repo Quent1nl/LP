b85ce4246b7c4dc567801508ef8404ba673dcc7d
Quent1nl <56486588+Quent1nl@users.noreply.github.com>
2020-03-11 20:38:27 +0100

    Modification json

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,7 +1,10 @@
 class Burger < ApplicationRecord
     def nutriments
-        code = "3029330003533"
-        product = Openfoodfacts::Product.get(code, locale: 'fr')
-        return product.nutriments
+        product = Openfoodfacts::Product.get(self.code, locale: 'fr')
+        if product
+            return product.nutriments.to_hash
+        else
+            return ["nutriment" => "Could not retrieve nutriments, product code must be erroned"]
+        end
     end
 end
