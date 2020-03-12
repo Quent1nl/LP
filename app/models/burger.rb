b2083c5ee0edc621c5ebc18fda296ffba0fbe8da
Quent1nl <56486588+Quent1nl@users.noreply.github.com>
2020-03-12 09:26:08 +0100

    Modif du json

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,10 +1,34 @@
 class Burger < ApplicationRecord
     def nutriments
         product = Openfoodfacts::Product.get(self.code, locale: 'fr')
-        if product
-            return product.nutriments.to_hash
+        if(@product) 
+            @nutriments = @product.nutriments
+            #p @nutriments
+            @resultset = [
+                "salt" => @nutriments.salt,
+                "salt_unit" => @nutriments.salt_unit, 
+                "sugars" => @nutriments.sugars,
+                "sugars_unit" => @nutriments.sugars_unit,
+                "proteins" => @nutriments.proteins,
+                "proteins_unit" => @nutriments.proteins_unit,
+                "sodium" => @nutriments.sodium,
+                "sodium_unit" => @nutriments.sodium_unit,
+                'energy' => @nutriments.energy,
+                'energy_unit' => @nutriments.energy_unit
+            ]
         else
-            return ["nutriment" => "Could not retrieve nutriments, product code must be erroned"]
+            @resultset = [
+                "salt" => "",
+                "salt_unit" => "", 
+                "sugars" => "",
+                "sugars_unit" => "",
+                "proteins" => "",
+                "proteins_unit" => "",
+                "sodium" => "",
+                "sodium_unit" => "",
+                'energy' => "",
+                'energy_unit' => "",
+            ]
         end
     end
 end
