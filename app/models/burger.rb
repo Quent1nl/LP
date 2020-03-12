ea5da8eddd906048ee92e67e7c7cb972527ae8ec
Quent1nl <56486588+Quent1nl@users.noreply.github.com>
2020-03-12 17:51:12 +0100

    Ajout du CSS

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,34 +1,10 @@
 class Burger < ApplicationRecord
     def nutriments
         product = Openfoodfacts::Product.get(self.code, locale: 'fr')
-        if(@product) 
-            @nutriments = @product.nutriments
-            #p @nutriments
-            @resultset = [
-                "salt" => @nutriments.salt,
-                "salt_unit" => @nutriments.salt_unit, 
-                "sugars" => @nutriments.sugars,
-                "sugars_unit" => @nutriments.sugars_unit,
-                "proteins" => @nutriments.proteins,
-                "proteins_unit" => @nutriments.proteins_unit,
-                "sodium" => @nutriments.sodium,
-                "sodium_unit" => @nutriments.sodium_unit,
-                'energy' => @nutriments.energy,
-                'energy_unit' => @nutriments.energy_unit
-            ]
+        if product
+            return product.nutriments.to_hash
         else
-            @resultset = [
-                "salt" => "",
-                "salt_unit" => "", 
-                "sugars" => "",
-                "sugars_unit" => "",
-                "proteins" => "",
-                "proteins_unit" => "",
-                "sodium" => "",
-                "sodium_unit" => "",
-                'energy' => "",
-                'energy_unit' => "",
-            ]
+            return ["nutriment" => "Could not retrieve nutriments, product code must be erroned"]
         end
     end
 end
